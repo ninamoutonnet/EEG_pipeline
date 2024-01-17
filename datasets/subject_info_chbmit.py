cde9f446e0bf74a0658510f81c8abcbb845e5503
Nina Moutonnet <70032219+ninamoutonnet@users.noreply.github.com>
2024-01-17 17:00:32 +0000

    Create subject_info_chbmit.py

---
+++ datasets/subject_info_chbmit.py
@@ -0,0 +1,33 @@
+'''
+This file contains the information about the subjects of the CHB-MIT dataset. The subject ID is followed by the sex and age (years) of the subject.
+See: https://physionet.org/content/chbmit/1.0.0/SUBJECT-INFO
+
+Note: Case chb24 was added to this collection in December 2010, and is not currently included in SUBJECT-INFO.
+
+'''
+
+subject_info_CHBMIT = {'chb01':['F', 11],
+                        'chb02':['M', 11],
+                        'chb03':['F', 14],
+                        'chb04':['M', 22],
+                        'chb05':['F', 7],
+                        'chb06':['F', 1.5],
+                        'chb07':['F', 14.5],
+                        'chb08':['M', 3.5],
+                        'chb09':['F', 10],
+                        'chb10':['M', 3],
+                        'chb11':['F', 12],
+                        'chb12':['F', 2],
+                        'chb13':['F', 3],
+                        'chb14':['F', 9],
+                        'chb15':['M', 16],
+                        'chb16':['F', 7],
+                        'chb17':['F', 12],
+                        'chb18':['F', 18],
+                        'chb19':['F', 19],
+                        'chb20':['F', 6],
+                        'chb21':['F', 13],
+                        'chb22':['F', 9],
+                        'chb23':['F', 6],
+                        'chb24':['N/A', 'N/A']
+                       }
